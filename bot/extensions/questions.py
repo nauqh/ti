a6c34dc8f1256c967d560610f32f0d3148ef1ff0
Nauqh <hodominhquan.work@gmail.com>
2024-12-18 15:46:06 +0700

    feat(scraper): add follow-up 2 response

--- bot/extensions/questions.py
+++ bot/extensions/questions.py
@@ -19,9 +19,7 @@ async def on_starting(event: hikari.StartingEvent) -> None:
     with open("instructions.txt", "r") as file:
         instructions = file.read()
     bot.create_vector_store(["docs/instructions.pdf", "docs/user_manual.pdf"])
-    bot.create_assistant(
-        instructions=instructions
-    )
+    bot.create_assistant(instructions=instructions)
     plugin.app.d.bot = bot
 
 
@@ -38,10 +36,14 @@ async def on_thread_create(event: hikari.GuildThreadCreateEvent) -> None:
             message: hikari.Message = messages[0]
 
             images = [
-                att.url for att in message.attachments if att.media_type.startswith("image")
+                att.url
+                for att in message.attachments
+                if att.media_type.startswith("image")
             ]
             attachments = [
-                att.url for att in message.attachments if not att.media_type.startswith("image")
+                att.url
+                for att in message.attachments
+                if not att.media_type.startswith("image")
             ]
 
             try:
@@ -50,7 +52,7 @@ async def on_thread_create(event: hikari.GuildThreadCreateEvent) -> None:
                 ds_thread = bot.create_thread(
                     user_message=message.content,
                     image_urls=images,
-                    file_paths=attachments
+                    file_paths=attachments,
                 )
 
                 messages = bot.create_and_run_thread(thread=ds_thread)
@@ -68,28 +70,40 @@ async def on_thread_create(event: hikari.GuildThreadCreateEvent) -> None:
             f"Thread {thread.name} does not belong to the question center category.")
 
 
-# @plugin.listener(hikari.GuildMessageCreateEvent)
-# async def on_message_create(event: hikari.GuildMessageCreateEvent) -> None:
-#     """
-#     Handles new messages in the question center thread to respond to follow-up questions.
-#     """
-#     message = event.message
-#     if message.author.is_bot:
-#         return
+@plugin.listener(hikari.GuildMessageCreateEvent)
+async def on_message_create(event: hikari.GuildMessageCreateEvent) -> None:
+    """
+    Handles new messages in the question center thread to respond to follow-up questions.
+    """
+    message = event.message
+    if message.author.is_bot:
+        return
 
-#     try:
-#         thread: hikari.GuildThreadChannel = await message.fetch_channel()
-#         if len(await thread.fetch_history()) <= 1:
-#             return
+    try:
+        thread: hikari.GuildThreadChannel = await message.fetch_channel()
+        if len(await thread.fetch_history()) <= 1:
+            return
+        if (
+            len(
+                [
+                    message
+                    for message in await thread.fetch_history()
+                    if message.author.id == 1316322317889962014
+                ]
+            )
+            >= 2
+        ):
+            logger.info(f"2 responses found, stop follow-up {thread.name}")
+            return
 
-#         if thread.parent_id == int(os.environ["TEST_FORUM"]):
-#             bot = plugin.app.d.bot
+        if thread.parent_id == int(os.environ["TEST_FORUM"]):
+            bot = plugin.app.d.bot
 
-#             response, citations = bot.continue_conversation(message.content)
-#             await thread.send(response)
+            response, citations = bot.continue_conversation(message.content)
+            await thread.send(response)
 
-#             if citations:
-#                 logger.info(f"Referenced files: {', '.join(citations)}")
+            if citations:
+                logger.info(f"Referenced files: {', '.join(citations)}")
 
-#     except Exception as e:
-#         logger.error(f"An error occurred: {e}")
+    except Exception as e:
+        logger.error(f"An error occurred: {e}")
