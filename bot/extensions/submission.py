982416839b1b21cd7f307d6bbbff9f594c9836b0
Nauqh <hodominhquan.work@gmail.com>
2025-03-24 17:18:35 +1100

    fix(submission.py): change log message

--- bot/extensions/submission.py
+++ bot/extensions/submission.py
@@ -271,11 +271,10 @@ async def handle_websocket(uri: str, channel_id: int):
                                     print(
                                         f"Error deleting temp file: {str(e)}")
 
-                        # NOTE: TEST: Send a message to DS server
                         await plugin.bot.rest.create_message(
                             1237424754739253279,
                             (
-                                f"@everyone New submission added\n"
+                                f"New submission from `content['email']`\n"
                                 f"- Exam: {content['exam_name']}\n"
                                 f"- Email: {content['email']}\n"
                                 f"View submission at https://csassessment.it.com/marking/{content['submission_id']}"
